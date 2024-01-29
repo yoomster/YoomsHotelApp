0061b6336fa0ef91d7593d8bd84d2f2132d1e51b
yoomster <nperenboom@hotmail.com>
2024-01-29 16:41:00 +0100

    Library added to the solution

---
+++ YoomsHotelLibrary/Data/DataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoomsHotelLibrary.Data
+{
+    public class DataAccess
+    {
+        //generic load and save data
+
+
+    }
+}
